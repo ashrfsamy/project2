39d1b0e32e56d5d23ebdf85760b84166624fb20e
ashrfsamy <ashrfsamy200211@gmail.com>
2022-08-18 01:01:53 +0200

    Initial commit

---
+++ js/myjava.js
@@ -0,0 +1,12 @@
+let prg=document.getElementsByClassName("p");
+let bu=document.getElementsByClassName("b");
+
+bu[0].onclick=function()
+{
+
+if (bu[0].textContent=="Get Started"){
+    prg[0].style="display:block";
+    bu[0].textContent="Get Started";}
+    
+}
+
